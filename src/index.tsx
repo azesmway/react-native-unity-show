dab3bd5cd51b7de77c9627a05a82e9628f725cb1
azesm <azesm@me.com>
2021-12-22 10:22:26 +0300

    Update code 3 test

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import {
-  requireNativeComponent,
-  UIManager,
-  Platform,
-  ViewStyle,
+  // requireNativeComponent,
+  // UIManager,
+  // Platform,
+  // ViewStyle,
   View,
   Text,
 } from 'react-native';
 import DeviceInfo from 'react-native-device-info';
 
-const LINKING_ERROR =
-  `The package 'react-native-unity-show' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo managed workflow\n';
-
-type UnityShowProps = {
-  color: string;
-  style: ViewStyle;
-};
-
-const ComponentName = 'UnityShowView';
+// const LINKING_ERROR =
+//   `The package 'react-native-unity-show' doesn't seem to be linked. Make sure: \n\n` +
+//   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+//   '- You rebuilt the app after installing the package\n' +
+//   '- You are not using Expo managed workflow\n';
+//
+// type UnityShowProps = {
+//   color: string;
+//   style: ViewStyle;
+// };
+//
+// const ComponentName = 'UnityShowView';
 
 // const UnityShowView =
 //   UIManager.getViewManagerConfig(ComponentName) != null
